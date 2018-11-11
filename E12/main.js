a0efff20670b2f8c6673cafce488e53509b6f0ee
Matthew Hazelworth <mhazelwo@masonlive.gmu.edu>
2018-11-10 20:40:09 -0500

    4

--- E12/main.js
+++ E12/main.js
@@ -1,26 +1,33 @@
-$(document).ready(function() 
-{
-    demoFunction();
-	
-    $(window).resize(function() 
-	{
-       
-    });
-	
-    $(window).scroll(function()
-	{
- 
-    });
-});
- 
-function demoFunction()
- {
-    $('p').addClass('bigger');
-    console.log('added bigger');
-    $('p').click(function()
-	{
-        $('p').css('color', 'white');
-        console.log('added white');
-        $('body').toggleClass('bodyBG');
-    });
-}
+/*global $,jQuery,console,window*/
+(function ($) {
+    "use strict";
+    var slideshow = (function () {
+        var counter = 0,
+            i,
+            j,
+            slides =  $("#slideshow .slide"),
+            slidesLen = slides.length - 1;
+        for (i = 0, j = 9999; i < slides.length; i += 1, j -= 1) {
+            $(slides[i]).css("z-index", j);
+        }
+        return {
+            startSlideshow: function () {
+                window.setInterval(function () {
+                    if (counter === 0) {
+                        slides.eq(counter).fadeOut();
+                        counter += 1;
+                    } else if (counter === slidesLen) {
+                        counter = 0;
+                        slides.eq(counter).fadeIn(function () {
+                            slides.fadeIn();
+                        });
+                    } else {
+                        slides.eq(counter).fadeOut();
+                        counter += 1;
+                    }
+                }, 2000);
+            }
+        };
+    }());
+    slideshow.startSlideshow();
+}(jQuery));
