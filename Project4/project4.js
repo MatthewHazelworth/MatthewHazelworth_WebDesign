1d29fc25a270958c2d12579bbf858c583df27d39
Matthew Hazelworth <mhazelwo@masonlive.gmu.edu>
2018-12-04 10:57:40 -0500

    a

---
+++ Project4/project4.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    // run function on initial page load
+    project4();
+	
+    // vPosH1();
+ 
+    // run function on resize of the window
+    $(window).resize(function() 
+	{
+      // vPosH1();
+    });
+ 
+    // run function on scroll
+    $(window).scroll(function() {
+        $('.jqClick').addClass('redC');
+    });
+});
+ 
+
+function project4() 
+{
+    $('.menu').click(function() {
+        $('.menu').toggleClass('hamburger');	        
+    })
+	
+    $('.numbers').hover(function() {
+        $(".numbers").toggleClass('blackText');
+  
+}
